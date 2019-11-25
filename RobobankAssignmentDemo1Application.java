2a303cb86669708bf4e2cfd1a36e51906683b245
rajshekar4 <58190374+rajshekar4@users.noreply.github.com>
2019-11-26 02:03:02 +0530

    Add files via upload

---
+++ RobobankAssignmentDemo1Application.java
@@ -0,0 +1,15 @@
+package com.rabobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages="com.*")
+@SpringBootApplication
+public class RobobankAssignmentDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobobankAssignmentDemo1Application.class, args);
+	}
+
+}
